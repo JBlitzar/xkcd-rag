6d51481fd4a1af733d346a62fd7fcd9e4e76976c
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-09-30 20:09:59 -0700

    Update embedding_search.py

--- embedding_search.py
+++ embedding_search.py
@@ -113,7 +113,9 @@ class EmbeddingCache:
         # Save atomically: write to temp files then move
         tmp_arr = self._combined_path() + ".tmp"
         tmp_idx = self._index_path() + ".tmp"
-        np.save(tmp_arr, arr)
+        # Use file handle so NumPy doesn't append .npy to tmp filename
+        with open(tmp_arr, "wb") as f:
+            np.save(f, arr)
         with open(tmp_idx, "w", encoding="utf-8") as f:
             json.dump(index, f, ensure_ascii=False, indent=2)
         os.replace(tmp_arr, self._combined_path())
