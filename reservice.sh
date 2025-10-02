d6e83d974b0c58b3159147edf58b185569e43a57
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-10-02 14:55:51 -0700

    reservice

---
+++ reservice.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp discord_bot.service ~/.config/systemd/user/discord-bot.service
+systemctl --user daemon-reload
+systemctl --user enable discord-bot
+systemctl --user restart discord-bot
+systemctl --user status discord-bot
+loginctl enable-linger $USER
+
+echo "if no work, try:"
+echo "export XDG_RUNTIME_DIR=/run/user/3277"
+echo "export DBUS_SESSION_BUS_ADDRESS=unix:path=\$XDG_RUNTIME_DIR/bus"
